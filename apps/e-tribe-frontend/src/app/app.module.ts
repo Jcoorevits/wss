bfa546e09f23d376a6b7d7855978681b4f8b0d27
Jeremy <r0843822@student.thomasmore.be>
2023-03-03 16:11:26 +0100

    wss

--- apps/e-tribe-frontend/src/app/app.module.ts
+++ apps/e-tribe-frontend/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {HttpClientModule} from "@angular/common/http";
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import {initializeKeycloak} from "../../../../libs/auth/src/lib/keycloak-init.factory";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    KeycloakAngularModule, HttpClientModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
